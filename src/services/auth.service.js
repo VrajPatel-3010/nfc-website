96b9f526cc04ebb3f2de5d7b4066e4c87c268096
Vraj Patel <patel.vraj7255@gmail.com>
2023-06-15 11:29:51 -0700

    Cross Site V 1.1.0

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "http://localhost:8080/api/auth/";
-const API_URL = "http://ec2-44-204-204-236.compute-1.amazonaws.com:8080/api/auth/";
+//const API_URL = "http://localhost:8080/api/";
+const API_URL = "http://ec2-44-204-204-236.compute-1.amazonaws.com:8080/api/";
 
 const register = (data) => {
   return axios.post(API_URL + "signup", {
@@ -14,7 +14,9 @@ const login = (email, password) => {
     .post(API_URL + "signin", {
       email,
       password,
-    })
+    },{headers: {
+      "Access-Control-Allow-Origin": '*',
+    }})
     .then((response) => {
       if (response.data.accessToken) {
         localStorage.setItem("user", JSON.stringify(response.data));
