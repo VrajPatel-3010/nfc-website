29ea6030daf9c13569c9dfbc4f5cd63f06495160
Vraj Patel <patel.vraj7255@gmail.com>
2023-06-15 10:55:57 -0700

    AXIOS CROSS SITE

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://ec2-44-204-204-236.compute-1.amazonaws.com:8080",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": '*',
   }
 });
