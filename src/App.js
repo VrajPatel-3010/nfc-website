114d61375ef5e5267e39c9d81b06617eea47ffb7
Vraj Patel <patel.vraj7255@gmail.com>
2023-12-11 15:24:34 -0800

    After Login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // routes
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import Router from './routes';
 // theme
 import ThemeProvider from './theme';
 // components
 import "./App.css";
 import Homepage from './components/HomePage';
 import Footer from './components/Footer';
-import $ from 'jquery';
 import { useSearchParams } from "react-router-dom";
 import ProfilePage from './components/ProfilePage';
 import AuthService from "./services/auth.service";
