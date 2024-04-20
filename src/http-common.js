54759085c387a8ae9659887a8409b609db4843ab
Vraj Patel <patel.vraj7255@gmail.com>
2024-04-19 19:20:21 -0700

    URL Change

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-44-204-212-251.compute-1.amazonaws.com:8080/api",
+  //baseURL: "http://ec2-44-204-212-251.compute-1.amazonaws.com:8080/api",
   //baseURL: "http://localhost:8080/api",
+  baseURL:  "https://nfc-backend-420900.de.r.appspot.com/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": '*',
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import authHeader from "./auth-header";
 
 //const API_URL = "http://localhost:8080/api/";
-const API_URL = "http://ec2-44-204-212-251.compute-1.amazonaws.com:8080/api/";
+//const API_URL = "http://ec2-44-204-212-251.compute-1.amazonaws.com:8080/api/";
+const API_URL = "https://nfc-backend-420900.de.r.appspot.com/api/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
